68502fb5501366fc84a2632006fd0eab9d1a5135
m4tiss <mateusz039@op.pl>
2022-01-05 13:12:44 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TestRepo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestRepo.dir/main.c.obj"
+  "TestRepo.exe"
+  "TestRepo.exe.manifest"
+  "TestRepo.pdb"
+  "libTestRepo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TestRepo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
